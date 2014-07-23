ec2cf37c24ed94d295b26536508f116f32b522f9
jhend11 <jhend11@uw.edu>
2014-07-23 13:11:07 -0400

    started tableviewcontroller

--- List App/List App/MFLAppDelegate.m
+++ List App/List App/MFLAppDelegate.m
@@ -7,6 +7,8 @@
 //
 
 #import "MFLAppDelegate.h"
+#import "MFLTableViewController.h"
+
 
 @implementation MFLAppDelegate
 
@@ -14,6 +16,13 @@
 {
     self.window = [[UIWindow alloc] initWithFrame:[[UIScreen mainScreen] bounds]];
     // Override point for customization after application launch.
+    
+    MFLTableViewController * tableVC = [[MFLTableViewController alloc] initWithStyle:UITableViewStylePlain];
+    
+    self.window.rootViewController = tableVC;
+    
+    
+    
     self.window.backgroundColor = [UIColor whiteColor];
     [self.window makeKeyAndVisible];
     return YES;
---
+++ List App/MFLTableViewController.h
@@ -0,0 +1,15 @@
+//
+//  MFLTableViewController.h
+//  List App
+//
+//  Created by JOSH HENDERSHOT on 7/23/14.
+//  Copyright (c) 2014 Joshua Hendershot. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface MFLTableViewController : UITableViewController
+
+
+
+@end
