36b7f2ec5cc3074fb2add8f1b306cc66b8f82180
jhend11 <jhend11@uw.edu>
2014-07-24 17:31:29 -0400

    Hella work on Github friends app

--- Github Friends/GFATableViewCell.m
+++ Github Friends/GFATableViewCell.m
@@ -13,6 +13,14 @@
     UILabel * friendName;
     
     UIImageView * friendImage;
+    
+    UILabel * friendLoc;
+    UILabel * followers;
+    UILabel * following;
+    
+    
+    UIButton * gist;
+    UIButton * profile;
 }
 
 - (id)initWithStyle:(UITableViewCellStyle)style reuseIdentifier:(NSString *)reuseIdentifier
@@ -22,13 +30,65 @@
         // Initialization code
         
         
-        friendName = [[UILabel alloc]initWithFrame:CGRectMake(60, 10, 200, 40)];
+        self.layer.borderWidth = 0.25;
+        self.layer.borderColor = [[UIColor blackColor]CGColor];
         
+        
+//        cell.textLabel.text = githubFriends[indexPath.row][@"followers"];
+//        cell.textLabel.font = [UIFont fontWithName:@"HelveticaNeue-UltraLight" size:15];
+//        cell.textLabel.
+
+        followers = [[UILabel alloc] initWithFrame:CGRectMake(10, 50, 100, 20)];
+        [self.contentView addSubview:followers];
+        followers.font = [UIFont fontWithName:@"HelveticaNeue-Light"size:10];
+        
+        following = [[UILabel alloc] initWithFrame:CGRectMake(10, 60, 100, 20)];
+        [self.contentView addSubview:following];
+        following.font = [UIFont fontWithName:@"HelveticaNeue-Light"size:10];
+        
+        
+        friendLoc =[[UILabel alloc] initWithFrame:CGRectMake(120, 40, 200, 40)];
+        [self.contentView addSubview:friendLoc];
+        friendLoc.font = [UIFont fontWithName:@"HelveticaNeue-Light"size:15];
+        
+        
+        
+        
+        
+        friendName = [[UILabel alloc]initWithFrame:CGRectMake(90, 10, 200, 40)];
         [self.contentView addSubview:friendName];
+        friendName.font = [UIFont fontWithName:@"HelveticaNeue-Light"size:15];
+
+        
+        friendLoc = [[UILabel alloc]initWithFrame:CGRectMake(90, 40, 200, 40)];
+        [self.contentView addSubview:friendLoc];
+        friendLoc.font = [UIFont fontWithName:@"HelveticaNeue-Light"size:15];
+
         
         friendImage = [[UIImageView alloc]initWithFrame:CGRectMake(10, 10, 40, 40)];
         [self.contentView addSubview:friendImage];
+        friendImage.layer.cornerRadius = 20;
+        friendImage.layer.masksToBounds = 1;
         
+        
+        gist = [[UIButton alloc]initWithFrame:CGRectMake(220.0, 50, 80.0, 30)];
+        [gist setTitle:@"Gist" forState:UIControlStateNormal];
+        gist.backgroundColor = [UIColor colorWithRed:0.769f green:0.769f blue:0.769f alpha:1.0f];
+        [gist addTarget:self action:@selector(gistButtonWasClicked) forControlEvents:UIControlEventTouchUpInside];
+        gist.layer.cornerRadius = 15;
+        gist.titleLabel.font = [UIFont fontWithName:@"HelveticaNeue-Light"size:15];
+
+        [self.contentView addSubview:gist];
+        
+        
+        profile = [[UIButton alloc]initWithFrame:CGRectMake(220.0, 10, 80.0, 30)];
+        [profile setTitle:@"Profile" forState:UIControlStateNormal];
+        profile.backgroundColor = [UIColor colorWithRed:0.769f green:0.769f blue:0.769f alpha:1.0f];
+        [profile addTarget:self action:@selector(profileButtonWasClicked) forControlEvents:UIControlEventTouchUpInside];
+        profile.layer.cornerRadius = 15;
+        profile.titleLabel.font = [UIFont fontWithName:@"HelveticaNeue-Light"size:15];
+
+        [self.contentView addSubview:profile];
         
         
         
@@ -64,6 +124,19 @@
     friendName.text = friendInfo[@"login"];
     friendImage.image = image;
     
+    friendLoc.text = friendInfo[@"location"];
+//    following.text = friendInfo[@"following"];
+//    followers.text = friendInfo[@"followers"];
+    
+    
+    
+    NSString * followerString = [NSString stringWithFormat:@"Followers : %@", friendInfo[@"followers"]];
+    followers.text = followerString;
+    
+    NSString * followingString = [NSString stringWithFormat:@"Following : %@", friendInfo[@"following"]];
+    following.text = followingString;
+    
+    
     
     
     
@@ -72,7 +145,14 @@
 }
 
 
-
+-(void)profileButtonWasClicked
+{
+    NSLog(@"profileButton was clicked");
+}
+-(void)gistButtonWasClicked
+{
+    NSLog(@"gistButton was clicked");
+}
 
 
 - (void)awakeFromNib
--- Github Friends/GFATableViewController.m
+++ Github Friends/GFATableViewController.m
@@ -9,7 +9,7 @@
 #import "GFATableViewController.h"
 #import "GFATableViewCell.h"
 
-@interface GFATableViewController ()
+@interface GFATableViewController () <UITextFieldDelegate>
 
 
 @end
@@ -18,6 +18,12 @@
 
 {
     NSMutableArray * githubFriends;
+    
+    UITextField * search;
+    
+    UIButton * find;
+    
+    
     
     
 }
@@ -34,28 +40,191 @@
         @{
                                
             @"login": @"jhend11",
-            @"id": @7279926,
             @"avatar_url": @"https://avatars.githubusercontent.com/u/7279926?",
+            @"location": @"Atlanta, GA",
+            @"followers": @"0",
+            @"following":@"0",
 
             },
-                           
-                           
-                        
+        @{
+            
+            @"login": @"Anterio",
+            @"avatar_url": @"https://avatars.githubusercontent.com/u/8224728?",
+            @"location": @"Atlanta, GA",
+            @"followers": @"0",
+            @"following":@"0",
+            
+            },
+        @{
+            
+            @"login": @"josephlausf",
+            @"avatar_url": @"https://avatars.githubusercontent.com/u/7451830?",
+            @"location": @"Atlanta, GA",
+            @"followers": @"0",
+            @"following":@"0",
+            
+            },
+        @{
+            
+            @"login": @"kalson",
+            @"avatar_url": @"https://avatars.githubusercontent.com/u/7114996?",
+            @"location": @"Atlanta, GA",
+            @"followers": @"0",
+            @"following":@"0",
+            
+            },
+        @{
+            
+            @"login": @"jeremycbutler",
+            @"avatar_url": @"https://avatars.githubusercontent.com/u/7903562?",
+            @"location": @"Atlanta, GA",
+            @"followers": @"0",
+            @"following":@"0",
+            
+            },
+        @{
+            
+            @"login": @"renecandelier",
+            @"avatar_url": @"https://avatars.githubusercontent.com/u/4494771?",
+            @"location": @"Atlanta, GA",
+            @"followers": @"0",
+            @"following":@"0",
+            
+            },
+        @{
+            
+            @"login": @"ericstephen",
+            @"avatar_url": @"https://avatars.githubusercontent.com/u/8224735?",
+            @"location": @"Atlanta, GA",
+            @"followers": @"0",
+            @"following":@"0",
+            
+            },
+        @{
+            
+            @"login": @"schwaebek",
+            @"avatar_url": @"https://avatars.githubusercontent.com/u/6909470?",
+            @"location": @"Atlanta, GA",
+            @"followers": @"0",
+            @"following":@"0",
+            
+            },
+        @{
+            
+            @"login": @"ewjseidel",
+            @"avatar_url": @"https://avatars.githubusercontent.com/u/8224720?",
+            @"location": @"Atlanta, GA",
+            @"followers": @"0",
+            @"following":@"0",
+            
+            },
+        @{
+            
+            @"login": @"npeterson213",
+            @"avatar_url": @"https://avatars.githubusercontent.com/u/8224722?",
+            @"location": @"Atlanta, GA",
+            @"followers": @"0",
+            @"following":@"0",
+            
+            },
+        @{
+            
+            @"login": @"dmerrill88",
+            @"avatar_url": @"https://avatars.githubusercontent.com/u/8224723?",
+            @"location": @"Atlanta, GA",
+            @"followers": @"0",
+            @"following":@"0",
+            
+            },
+        @{
+            
+            @"login": @"jaimeconnor",
+            @"avatar_url": @"https://avatars.githubusercontent.com/u/8224727?",
+            @"location": @"Atlanta, GA",
+            @"followers": @"0",
+            @"following":@"0",
+            
+            },
+        
                           
                            
-                           ] mutableCopy];
+        ] mutableCopy];
         
         
     }
     return self;
 }
 
+
+
+-(void)dismissKeyboard
+{
+    [search resignFirstResponder];
+}
+
+
 - (void)viewDidLoad
 {
+    
+    
+    
+    int screenWidth = [[UITableViewHeaderFooterView alloc]init].bounds.size.height;
+
+    
     
     UIView * headerView = [[UIView alloc]initWithFrame:CGRectMake(0, 0, 320, 100)];
     
     self.tableView.tableHeaderView = headerView;
+    
+    
+    
+    search = [[UITextField alloc] initWithFrame:CGRectMake(screenWidth + 20, 35,220, 45.0)];
+    search.backgroundColor = [UIColor colorWithRed:1.000f green:1.000f blue:1.000f alpha:1.0f];
+    search.autocapitalizationType = UITextAutocapitalizationTypeNone;
+    search.autocapitalizationType = UITextAutocorrectionTypeNo;
+    search.placeholder = @" search";
+    search.font = [UIFont fontWithName:@"HelveticaNeue-UltraLight" size:30];
+    UIView *paddingView = [[UIView alloc] initWithFrame:CGRectMake(0, 0, 10, 20)];
+    search.leftView= paddingView;
+    
+    search.leftViewMode = UITextFieldViewModeAlways;
+    search.layer.borderColor = [UIColor blackColor].CGColor;
+    search.layer.borderWidth = 1;
+    
+    search.delegate = self;
+    
+    [self.view addSubview:search];
+    
+    
+    
+    find = [[UIButton alloc]initWithFrame:CGRectMake(screenWidth + 260.0, 30, 50.0, 50)];
+    find.backgroundColor = [UIColor lightGrayColor];
+    [find setTitle:@"?" forState:UIControlStateNormal];
+    [find addTarget:self action:@selector(findButtonWasClicked) forControlEvents:UIControlEventTouchUpInside];
+    find.layer.cornerRadius = 25;
+    [self.view addSubview:find];
+
+//    UIButton * buttonPad = [[UIButton alloc] initWithFrame:CGRectMake(0, 0, 35, 35)];
+//    find.rightView = buttonPad;
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    UITapGestureRecognizer *tap = [[UITapGestureRecognizer alloc]
+                                   initWithTarget:self
+                                   action:@selector(dismissKeyboard)];
+    
+    [self.view addGestureRecognizer:tap];
+    [tap setCancelsTouchesInView:NO];
+    
+    
+    
+    
     
     [super viewDidLoad];
     
@@ -65,6 +234,26 @@
     // Uncomment the following line to display an Edit button in the navigation bar for this view controller.
     // self.navigationItem.rightBarButtonItem = self.editButtonItem;
 }
+
+
+
+
+-(void)findButtonWasClicked
+{
+    NSLog(@"findButton was clicked");
+}
+
+
+
+
+- (BOOL)textFieldShouldReturn:(UITextField *)textField
+{
+    [textField resignFirstResponder];
+    return YES;
+}
+
+
+
 
 - (void)didReceiveMemoryWarning
 {
@@ -84,7 +273,7 @@
 
 - (CGFloat)tableView:(UITableView *)tableView heightForRowAtIndexPath:(NSIndexPath *)indexPath
 {
-    return 60;
+    return 90;
 }
 
 - (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section
@@ -104,11 +293,14 @@
     if (cell == nil)
     {
         cell = [[GFATableViewCell alloc]initWithStyle:UITableViewCellStyleDefault reuseIdentifier:@"cell"];
-                
+//        cell.textLabel =[(@"login",githubFriends)];
+        
     }
     
     
     cell.friendInfo = githubFriends[indexPath.row];
+    cell.backgroundColor = [UIColor colorWithRed:1.000f green:1.000f blue:1.000f alpha:1.0f];
+    
     
     
     
