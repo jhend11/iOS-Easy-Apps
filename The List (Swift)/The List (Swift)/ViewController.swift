9c677a6c3feb6b297e7987cdb9c7054937ac9dc2
jhend11 <jhend11@uw.edu>
2014-08-26 14:49:08 -0400

    Work in progress

--- The List (Swift)/The List (Swift)/ViewController.swift
+++ The List (Swift)/The List (Swift)/ViewController.swift
@@ -10,14 +10,17 @@ import UIKit
 
 class ViewController: UIViewController,UITableViewDataSource,UITableViewDelegate {
     
-   let niceArray = ["Helper Robots","Some Humans"]
-   let naughtyArray = ["Invading Aliens","Killer Robots","Jaws"]
+    var niceArray = ["Helper Robots","Some Humans"]
+    var naughtyArray = ["Invading Aliens","Killer Robots","Jaws"]
+    let tabBarC = UITabBarController()
+
+    let listNameField = UITextField(frame: CGRectMake(10, 10, UIScreen.mainScreen().bounds.size.width - 20, 40))
+    let listOption = UISegmentedControl(frame: CGRectMake(10, 60, UIScreen.mainScreen().bounds.size.width - 20, 40))
     
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
         
-        let tabBarC = UITabBarController()
         self.view.addSubview(tabBarC.view)
         self.addChildViewController(tabBarC)
         
@@ -27,6 +30,7 @@ class ViewController: UIViewController,UITableViewDataSource,UITableViewDelegate
         niceList.tableView.delegate = self
         niceList.tableView.dataSource = self
         niceList.tableView.tag = 0
+        niceList.tabBarItem.image = UIImage(named: "nice")
         niceList.tableView .registerClass(UITableViewCell.self, forCellReuseIdentifier: "cell")
         
         let naughtyList = UITableViewController()
@@ -34,14 +38,62 @@ class ViewController: UIViewController,UITableViewDataSource,UITableViewDelegate
         naughtyList.tableView.delegate = self
         naughtyList.tableView.dataSource = self
         naughtyList.tableView.tag = 1
+        naughtyList.tabBarItem.image = UIImage(named: "naughty")
+
         naughtyList.tableView .registerClass(UITableViewCell.self, forCellReuseIdentifier: "cell")
         
-        tabBarC.viewControllers = [niceList, naughtyList]
+        let addToList = UIViewController()
+        addToList.title = "Add To List"
+        
+       
+        listNameField.layer.cornerRadius = 10
+        listNameField.backgroundColor = UIColor.lightGrayColor()
+        addToList.view.addSubview(listNameField)
+        
+        
+        listOption.tintColor = UIColor.lightGrayColor()
+        listOption.insertSegmentWithTitle("Nice", atIndex: 0, animated: true)
+        listOption.insertSegmentWithTitle("Naughty", atIndex: 1, animated: true)
+        addToList.view.addSubview(listOption)
+        
+        let addButton = UIButton(frame: CGRectMake(10, 110, UIScreen.mainScreen().bounds.size.width, 40))
+        addButton.backgroundColor = addButton.tintColor
+        addButton.tintColor = UIColor.whiteColor()
+        addButton.setTitle("Add New Item", forState: .Normal)
+        addButton.addTarget(self, action: Selector("addNewItem"), forControlEvents: .TouchUpInside)
+        
+        addToList.view.addSubview(addButton)
+        
+        tabBarC.viewControllers = [niceList, naughtyList,addToList]
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
+    }
+    
+    func addNewItem()
+    {
+//        var itemArray = (Bool(listOption.selectedSegmentIndex)) ? naughtyArray : niceArray
+//        
+//        itemArray += [listNameField.text]
+        
+        switch(listOption.selectedSegmentIndex)
+            {
+        case 0:
+            niceArray += [listNameField.text]
+        case 1:
+            naughtyArray.append(listNameField.text)
+        default:
+            print("default")
+        }
+        listNameField.text = ""
+        listNameField.resignFirstResponder()
+
+//        listNameField.becomeFirstResponder()
+        
+        (tabBarC.viewControllers[listOption.selectedSegmentIndex] as UITableViewController).tableView.reloadData()
+        
     }
     
     func tableView(tableView: UITableView!, numberOfRowsInSection section: Int) -> Int
